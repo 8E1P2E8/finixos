b48c27d7a5c58ad7ac00a1e11514eca79f3a08af
Vitrial <xzecora@gmail.com>
2026-04-21 17:21:31 -0400

    initial commit

---
+++ finix/configuration.nix
@@ -0,0 +1,138 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  #boot.loader.grub.enable = false;
+  #boot.loader.grub.device = "/dev/vda";
+  #boot.loader.grub.useOSProber = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  finit.runlevel = 3;
+
+  finit.services.nix-daemon = {
+    environment.CURL_CA_BUNDLE = config.security.pki.caBundle;
+    path = [
+      config.services.nix-daemon.package
+      pkgs.util-linux
+      config.services.openssh.package
+    ];
+  };
+
+  services.nix-daemon = {
+    enable = true;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+    };
+  };
+
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  programs.limine = {
+    enable = true;
+    settings.editor_enabled = true;
+    force = true;
+  };
+
+  programs.sudo.enable = true;
+
+  programs.bash.enable = true;
+
+  services.polkit.enable = true;
+
+  services.mdevd.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  #networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  #services.xserver.xkb = {
+  #  layout = "us";
+  #  variant = "";
+  #};
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vitrial = {
+    isNormalUser = true;
+    description = "vitrial";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  #nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  #system.stateVersion = "25.11"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Nixos VM flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    finix.url = "github:finix-community/finix?tag=finit-4.16";
+  };
+
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    finix,
+    ...
+  }: 
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+  in {
+    nixosConfigurations.nixos = finix.lib.finixSystem {
+      inherit (pkgs) lib;
+
+      modules = with finix.nixosModules; [
+        {
+          nixpkgs.pkgs = nixpkgs.lib.mkDefault pkgs;
+        }
+        (./finix/configuration.nix)
+        nix-daemon
+        openssh
+        limine
+        sudo
+        polkit
+        getty
+        bash
+      ];
+
+      specialArgs = {
+        modulesPath = toString nixpkgs + "/nixos/modules";
+      };
+    };
+  };
+}
