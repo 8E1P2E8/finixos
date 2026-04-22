5d163f3039bf72a5dbf004358d8e60dc2807dd97
vitrial <xzecora@gmail.com>
2026-04-22 18:41:09 -0400

    minimized

--- finix/configuration.nix
+++ finix/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -17,11 +13,11 @@
 
   finit.services.nix-daemon = {
     environment.CURL_CA_BUNDLE = config.security.pki.caBundle;
-    path = [
-      config.services.nix-daemon.package
-      pkgs.util-linux
-      config.services.openssh.package
-    ];
+    #path = [
+    #  config.services.nix-daemon.package
+    #  pkgs.util-linux
+    #  config.services.openssh.package
+    #];
   };
 
   services.nix-daemon = {
@@ -65,26 +61,8 @@
 
   networking.hostName = "finixos"; # Define your hostname.
 
-  # Enable networking
-  #networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vitrial = {
@@ -94,9 +72,6 @@
     password = "$6$1aOsu4xRRBDJWA3O$yUIEmHIzcJ2KczaW1RcVc6ji.vtCXND57iIqt8NfZHL7326zAViJrTGZriK.e1/5JovKqh/wElp7VmQB2TbLA.";
     packages = with pkgs; [];
   };
-
-  # Allow unfree packages
-  #nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -108,23 +83,4 @@
     iputils
     iproute2
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 }
--- finix/hardware-configuration.nix
+++ finix/hardware-configuration.nix
@@ -5,9 +5,9 @@
 
 {
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   hardware.firmware = [ pkgs.linux-firmware ];
